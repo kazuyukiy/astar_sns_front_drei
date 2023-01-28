b014c4e7d7b58dbc0f5d136c0f6cfe00f6f05bc2
kazuyukiy <tsuzuki.kazuyuki@gmail.com>
2023-01-29 07:50:06 +0900

    add autoprefixer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+    mode: "jit",
+    content: [
+	"./pages/**/*.{js,ts,jsx,tsx}",
+	"./components/**/*.{js,ts,jsx,tsx}",
+	"./utils/**/*.{js,ts,jsx,tsx}",
+	"./hooks/**/*.{js,ts,jsx,tsx}",
+    ],
   theme: {
     extend: {},
   },
